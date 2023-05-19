5329fe72f7c7e7994a80ae172c8bfde838b26380
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-19 04:07:19 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryItems(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+
+function createGalleryItems(galleryItems) {
+
+    const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+
+        return `
+            <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+
+    }).join('');
+
+    return galleryMarkup;
+}
+
+
+
+
+// console.log(galleryItems);
